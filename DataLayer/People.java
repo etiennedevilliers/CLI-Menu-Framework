92486e0e534ae2e6d415c90ae3af7ccf2f63242f
Etienne de Villiers <etiennefdevilliers@gmail.com>
2021-02-24 09:30:44 +0200

    minor changes

--- DataLayer/People.java
+++ DataLayer/People.java
@@ -3,5 +3,6 @@ package DataLayer;
 import java.util.ArrayList;
 
 public class People extends ArrayList<Person> {
+    private static final long serialVersionUID = 1L;
     
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 import MenuFramework.*;
 import AppFunctions.*;
 import DataLayer.*;
